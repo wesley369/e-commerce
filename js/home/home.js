4fa63f6e8596eea908528431fff953d1abf6bffd
wesley <wesley201075@gmail.com>
2023-06-04 19:06:10 -0300

    oitavo commit

--- js/home/home.js
+++ js/home/home.js
@@ -11,11 +11,11 @@ menuToggle.addEventListener('click', () => {
 function handleIntersection(entries) {
     entries.forEach(function (entry) {
       if (entry.intersectionRatio > 0) {
-        // Elemento está visível na tela
+        
         entry.target.classList.add('visible');
         observer.unobserve(entry.target);
 
-        // Carregar o próximo carrossel
+        
         var nextCarousel = entry.target.nextElementSibling;
         if (nextCarousel && nextCarousel.classList.contains('carrousel')) {
           observer.observe(nextCarousel);
@@ -31,11 +31,8 @@ function handleIntersection(entries) {
     observer.observe(carousel);
   });
 
-  $('#ofertas').slick({
-    // Configurações do Slick Carousel para ofertas do dia
-  });
-
-  // Inicializar apenas o primeiro carrossel
+ 
+  
   observer.observe(document.getElementById('ofertas'));
 
 
