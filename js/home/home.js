9bcb5d1084e0b6b6406d9f213da7622ba424cd3b
wesley <wesley201075@gmail.com>
2023-06-12 17:47:59 -0300

    22 commit

--- js/home/home.js
+++ js/home/home.js
@@ -33,6 +33,41 @@ function disableDarkMode() {
   localStorage.setItem('darkMode', 'false');
 }
 
+let fonteOriginal = {};
+
+const aumentarFonteBtn = document.getElementById("aumentarFonteBtn");
+aumentarFonteBtn.addEventListener("click", aumentarFonte);
+
+function aumentarFonte() {
+  const elementos = document.querySelectorAll("h1, h2, h3, h4, h5, h6, p, a, input[placeholder]");
+  
+  if (isEmpty(fonteOriginal)) {
+    // Salva as fontes originais
+    elementos.forEach(elemento => {
+      fonteOriginal[elemento.tagName] = parseInt(getComputedStyle(elemento).fontSize);
+    });
+  }
+  
+  elementos.forEach(elemento => {
+    const tagName = elemento.tagName;
+    const fontSize = parseInt(getComputedStyle(elemento).fontSize);
+    
+    if (fontSize === fonteOriginal[tagName]) {
+      // Aumenta a fonte em 10px
+      elemento.style.fontSize = (fontSize + 3) + "px";
+    } else {
+      // Retorna à fonte original
+      elemento.style.fontSize = fonteOriginal[tagName] + "px";
+    }
+  });
+}
+
+function isEmpty(obj) {
+  return Object.keys(obj).length === 0;
+}
+
+
+
 function handleIntersection(entries) {
     entries.forEach(function (entry) {
       if (entry.intersectionRatio > 0) {
